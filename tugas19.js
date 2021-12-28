bbf464e86313e3b52d6a068852935fedb2ccbd07
Josh0810 <92926113+Josh0810@users.noreply.github.com>
2021-12-29 02:12:35 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function Tugas19() {
+    var a = "Semangat...";
+    var b = "*";
+    console.log(a.repeat(5))
+    console.log("")
+    console.log(b.repeat(5))
+    console.log(b.repeat(20))
+    console.log(b.repeat(35))
+    console.log(b.repeat(45))
+    console.log(b.repeat(55))
+    console.log(b.repeat(45))
+    console.log(b.repeat(35))
+    console.log(b.repeat(20))
+    console.log(b.repeat(5))
+}
+Tugas19()
